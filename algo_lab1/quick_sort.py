f8b9efc410eb9dd67029b2fbbc3670f08b6e8c53
Oleksandr <alextsisinsky@gmail.com>
2021-09-14 16:08:12 +0300

    corrected tests, changed sort_order

--- algo_lab1/quick_sort.py
+++ algo_lab1/quick_sort.py
@@ -1,5 +1,7 @@
-import datetime
+import time
 import sys
+import os
+#from random import randint
 
 comparisons = 0
 swaps = 0
@@ -11,7 +13,7 @@ def swap(a, b, arr):
     swaps += 1
 
 
-def partition(array_for_quicksort, start, end):
+def partition(array_for_quicksort, start, end, order):
     pivot = array_for_quicksort[start]
     low = start + 1
     high = end
@@ -19,44 +21,49 @@ def partition(array_for_quicksort, start, end):
     global comparisons
     comparisons += 1
     while True:
-        while low <= high and array_for_quicksort[high] >= pivot:
-            high = high - 1
+        if order == "ASC":
+            while low <= high and array_for_quicksort[high] >= pivot:
+                high -= 1
+            while low <= high and array_for_quicksort[low] <= pivot:
+                low += 1
 
-        while low <= high and array_for_quicksort[low] <= pivot:
-            low = low + 1
-
+        elif order == "DESC":
+            while low <= high and array_for_quicksort[high] <= pivot:
+                high -= 1
+            while low <= high and array_for_quicksort[low] >= pivot:
+                low += 1
         if low <= high:
             swap(low, high, array_for_quicksort)
         else:
             break
-
     swap(start, high, array_for_quicksort)
-
     return high
 
 
 def quick_sort(array_for_quicksort, start, end, order):
     if start >= end:
         return
-    pi = partition(array_for_quicksort, start, end)
+    pi = partition(array_for_quicksort, start, end, order)
     quick_sort(array_for_quicksort, start, pi - 1, order)  # left partition
     quick_sort(array_for_quicksort, pi + 1, end, order)  # right partition
-
-    if order == "asc":
-        return array_for_quicksort
-    elif order == "desc":
-        return array_for_quicksort.reverse()
 
 
 if __name__ == '__main__':
     enter_array = sys.argv[1:-1]
     enter_asc_or_desc = sys.argv[-1]
     ready_array = [int(i) for i in enter_array]
-    start_time = datetime.datetime.now()
+
+    #my_list = [randint(1, 1000) for i in range(100)]
+
+    start_time = time.time()
     quick_sort(ready_array, 0, len(ready_array) - 1, enter_asc_or_desc)
-    time_sort = datetime.datetime.now() - start_time
+    end_time = time.time()
+
     print("QuickSort: \n"
-          f"Execution time: {time_sort.total_seconds() * 1000:.2f} ms\n"
+          f"Execution time: {(end_time - start_time) * 1000} ms\n"
           f"Comparisons: {comparisons} \n"
           f"Swaps: {swaps} \n"
           f"Sorted array: {ready_array}")
+
+    command = "python -m unittest test.TestQuickSort"
+    os.system(command)
